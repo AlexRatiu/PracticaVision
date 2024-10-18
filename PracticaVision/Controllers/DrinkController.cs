b28e1cbaaca6d049b1f1ed1ca12162ef187b86e5
AlexRatiu <117086170+AlexRatiu@users.noreply.github.com>
2024-10-18 13:13:53 +0300

    init

---
+++ PracticaVision/Controllers/DrinkController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PracticaVision.Controllers
+{
+    public class DrinkController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PracticaVision/Services/DrinkService.cs
@@ -0,0 +1,24 @@
+﻿public class DrinkService
+{
+    private Dictionary<int, string> context = new Dictionary<int, string> {
+            { 1, "Water" },
+            { 2, "Coffee" },
+            { 3, "Tea" },
+            { 4, "Juice" },
+            { 5, "Soda" },
+            { 6, "Beer" },
+            { 7, "Wine" }
+        };
+
+    public DrinkService() { }
+
+    public Dictionary<int, string> GetAll()
+    {
+        return context;
+    }
+
+    public string GetById(int id)
+    {
+        return context[id];
+    }
+}
