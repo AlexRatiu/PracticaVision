f6395218a35161f0580242668c2707be3424b4c5
AlexRatiu <117086170+AlexRatiu@users.noreply.github.com>
2024-10-18 14:57:35 +0300

    Controller

--- PracticaVision/Controllers/DrinkController.cs
+++ PracticaVision/Controllers/DrinkController.cs
@@ -1,12 +1,65 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using PracticaVision.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace PracticaVision.Controllers
 {
-    public class DrinkController : Controller
+    [ApiController]
+    [Route("[controller]")]
+    public class DrinkController
     {
-        public IActionResult Index()
+        private readonly DrinkService _drinkService;
+        public DrinkController(DrinkService drinkService)
         {
-            return View();
+            _drinkService = drinkService;
+        }
+
+        [HttpPost("[action]")]
+        public void AddDrink(Drink drink)
+        {
+           _drinkService.AddDrink(drink);
+        }
+
+        [HttpGet("[action]")]
+        public List<Drink> GetAllDrinks()
+        {
+            return _drinkService.GetAll();
+        }
+
+        [HttpGet("[action]")]
+        public Drink GetDrinkById(int id)
+        {
+            return _drinkService.GetById(id);
+        }
+
+        [HttpGet("[action]")]
+        public List<Drink> FindDrinkByName(string name)
+        {
+            return _drinkService.FindByName(name);
+        }
+
+        //Pentru cazul in care se cauta numele complet si este unic
+        /*[HttpGet("[action]")]
+        public Drink FindDrinkByName(string name)
+        {
+            return _drinkService.FindByName(name);
+        }*/
+
+        [HttpGet("[action]")]
+        public Drink GetRandomDrink()
+        {
+            return _drinkService.Random();
+        }
+
+        [HttpPut("[action]")]
+        public void UpdateDrink(Drink drink)
+        {
+            _drinkService.Update(drink);
+        }
+
+        [HttpDelete("[action]")]
+        public void DeleteDrink(int id)
+        {
+            _drinkService.DeleteDrink(id);
         }
     }
 }
